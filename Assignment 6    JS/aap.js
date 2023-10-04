d2ca297067815f4ebaf523d44cd9d27033104b91
uzair3999 <145483505+uzair3999@users.noreply.github.com>
2023-10-04 09:29:01 -0700

    Add files via upload

---
+++ Assignment 6    JS/aap.js
@@ -0,0 +1,73 @@
+// CHAPTER 35-38
+
+// Q1
+// var date = new Date()
+// console.log(date)
+
+// Q2
+// var firstName= prompt("Enter your first name?")
+// var lastName= prompt("Enter your last name?")
+// var fullName = firstName+" " +lastName;
+// console.log(fullName)
+
+// // Q3
+// var num1= +prompt("Enter your first value")
+// var num2= Number (prompt("Enter your second value"))
+// var add= num1 + num2;
+// console.log(add)
+
+// Q4
+// function calc(num1,operator,num2){
+//   if(operator ==="+"){
+//     return num1 + num2
+//   }
+//   else if(operator === "-"){
+//     return num1 - num2
+//   }
+//   else if (operator === "*"){
+//     return num1 * num2
+//   }
+    
+  
+//   else if( operator === "/"){
+//     return num1 / num2
+//   }
+// }
+// var num1 = +prompt("Enter your first number");
+// var operator = prompt("Enter your operator");
+// var num2 = +prompt("Enter your second number");
+// var result= calc (num1, operator, num2)
+// console.log(result)
+
+// Q5
+// function  popup(){
+//     alert("hello world")
+// }
+// popup ("hello world")
+
+// Q7
+// function multiplicationTable(tableNumber, tableLength) {
+//   for (var i = 1; i <= tableLength; i++) {
+//     document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br>");
+//   }
+// }
+
+// var tableNumber = +prompt("Enter a number to generate table...");
+// var tableLength = +prompt("Enter table length..");
+
+// multiplicationTable(tableNumber, tableLength);
+
+// Q10
+// var value = prompt("Enter a value for check")
+// var check= ""
+// for (var i=value.length-1; i>=0; i--){
+// console.log(value[i]);
+// check +=value[i];
+// }
+// if (value === check){
+//     console.log(value + "is a palindrome word");
+// }
+// else {
+//     console.log(value + "is not a palindrome word");
+// }
+
