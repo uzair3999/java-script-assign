429b6e8b4b954c9bbcef180dc2f62ee1abc89d99
uzair3999 <145483505+uzair3999@users.noreply.github.com>
2023-10-13 08:38:28 -0700

    Add files via upload

---
+++ Assignment 2   JS/aap.js
@@ -0,0 +1,33 @@
+// CHAPTER 9
+// Q 1
+
+var city= prompt ("type your city name")
+if(city === "karachi"){
+    console.log("welcome to city of lights")
+}
+
+// Q2
+var gender= prompt ("enter your gender ")
+if(gender === "male"){
+    console.log( "good morning sir")
+}
+else {
+    (gender === "female")
+    console.log ("good morning mam")
+}
+
+// Q3
+var signalcolor= prompt ("traffic color mean")
+if (signalcolor = "red"){
+    console.log ("must stop")
+}
+else {
+    (signalcolor ="yellow")
+    console.log("ready to move")
+}
+if (signalcolor = "green"){
+    console.log ("move now")
+}
+
+
+
