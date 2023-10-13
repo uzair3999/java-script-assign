761a0c62e5032626d1d4332c139ce0c422520c93
uzair3999 <145483505+uzair3999@users.noreply.github.com>
2023-10-13 08:41:17 -0700

    Add files via upload

---
+++ Assignment 4  JS/aap.js
@@ -0,0 +1,57 @@
+    //  CHAPTER 17
+// Q1
+for (i=1;  i<=10;  i++){
+    console.log (1)
+}
+
+// Q2
+for (i=1;  i<=10;  i++){
+    var arr="karachi, lahore, peshawar, multan, islamabad"
+    console.log (i+"    "+arr)
+}
+
+// Q3
+
+    
+
+
+// Q4
+for (var b=1; b<=100; b++){
+console.log(b)
+}
+
+// Q5
+for (var i=3; i>=3; i--){
+    console.log(i)
+}
+
+// Q6
+var arr="16,17,18,19,20"
+
+// Q7
+var flag=true
+
+// Q8
+for (i=1; i<=5; i++){
+    var arr="apple, bnana, graphs, orange,"
+    console.log(i+"  "+arr)
+}
+
+// Q9
+for (var i=0; i<=10; i++){
+    console.log (i)
+    if(i==1){
+        alert("the number is:"+i)
+    }
+}
+
+// Q10
+arr=prompt("enter your first name")
+for(i=1; i<=1; i++){
+    if(arr==="uzair"){
+        alert("Enter")
+    }
+    else
+    alert("please write correct user name")
+}
+
