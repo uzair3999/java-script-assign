d7d2f26fdf0cde7028fec6e316b20add0c235da3
uzair3999 <145483505+uzair3999@users.noreply.github.com>
2023-10-13 08:40:28 -0700

    Add files via upload

---
+++ Assignment 1    JS/aap.js
@@ -0,0 +1,107 @@
+// CHAPTER 1
+// Q1,2
+
+alert("         "+"          UZAIR")
+alert("         "+"          RAUF")
+alert("         "+"          uzairmemon3999@gmail.com")
+alert("         "+"          0320-0000000")
+alert("         "+"          ..........")
+
+// // Q3
+
+alert("         "+"          This is my Javascript web page...")
+
+// CHAPTER 2
+// Q1
+var firstName; 
+// Q2
+var lastName;  
+lastName = 6;
+console.log(lastName + lastName)
+// Q3
+var teamName = "Tiger";
+console.log (teamName)
+alert('gang:'+teamName )
+// Q4
+var bestMan= "charlie"
+console.log (bestMan)
+alert(''+bestMan)
+
+// CHAPTER 3
+// Q1
+var caseQty;
+// Q2
+var caseQty=144;
+console.log(caseQty)
+// Q3,4
+var num=9;
+num=2;
+console.log(num+num)
+// Q5
+var murchTotal=100;
+var shippingCharge=10;
+var totalAmount= murchTotal+shippingCharge;
+console.log(totalAmount)
+// Q6
+var num1=6;
+var num2=7;
+var totalNumbers = num1+num2;
+console.log(totalNumbers)
+
+// CHAPTER 4
+// Q1
+var productCost =3.45;
+// Q2
+var nameofBand;
+// Q3
+var firstNum=5;
+console.log(firstNum)
+// Q4
+var firstName=" "+"uzair";
+var lastName=" "+"rauf";
+var fullName = firstName+lastName;
+console.log(fullName)
+// Q5
+// Legalvariables
+1 firstName
+2 lastName
+3 num
+4 nameofBand
+5 totalValue
+
+// illegal variables
+1 iff;
+2 Cna;
+3 script;
+4 Dm
+
+// CHAPTER 5
+// Q1
+var Divide=20/4;
+console.log(Divide)
+// Q2
+var num=20%6;
+console.log(num)
+// Q3
+var largeNum= 1000*2000
+console.log (largeNum)
+multipication amount= 2000000
+// Q4
+var subtract1=20
+var subtract2=10
+console.log(subtract1-subtract2)
+// Q5
+var division
+division=18/3
+console.log(division)
+// Q6
+var multiply=2*4
+console.log(multiply)
+alert(**+multiply)
+                
+                    // END
+
+
+
+
+
